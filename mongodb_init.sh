b2131aa6f69d32ec258ba36d979040f1438a7153
Vincent <vincent@debian-BULLSEYE-live-builder-AMD64>
2026-04-19 17:44:12 +0200

    modification pour run auto avec nouveau mdp

--- mongodb_init.sh
+++ mongodb_init.sh
@@ -3,12 +3,12 @@ MONGODB_ADMIN_PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 20; echo)
 MONGODB_HOST="localhost"
 MONGODB_READER_USER="reader_user"
 MONGODB_READER_PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 20; echo)
-MONGO_DB_JSON="mongodb_init.json"
+MONGO_DB_JSON="mongodb_init.js"
 
 echo "use admin" > $MONGO_DB_JSON
 echo 'db.createUser({user:"'${MONGODB_ADMIN_USER}'",pwd:"'${MONGODB_ADMIN_PASSWORD}'",roles:[{role:"root",db:"admin"}]});' >> $MONGO_DB_JSON
 echo 'db.changeUserPassword("'${MONGODB_ADMIN_USER}'", "'${MONGODB_ADMIN_PASSWORD}'");' >> $MONGO_DB_JSON
-mongosh -f $MONGO_DB_JSON
+mongosh < $MONGO_DB_JSON
 
 sed -e "s/#######MONGODB_ADMIN_USER#######/${MONGODB_ADMIN_USER}/g" params.ini.model > params.ini
 sed -i -e "s/#######MONGODB_ADMIN_PASSWORD#######/${MONGODB_ADMIN_PASSWORD}/g" params.ini

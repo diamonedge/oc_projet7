5165bfc912226d4ee4242b0f577da515da28a40e
Vincent <vincent@debian-BULLSEYE-live-builder-AMD64>
2026-04-19 19:13:29 +0200

    ajout du rapport

--- js/rapport_questions.sh
+++ js/rapport_questions.sh
@@ -1,42 +1,35 @@
-echo "############# 1 - Affichez le premier document de la base de données pour observer ses champs"
+echo "############# 1 - Affichez le premier document de la base de données pour observer ses champs" > rapport.txt
 
-mongosh < 1_premier_dossier.js
+mongosh --quiet < js/1_premier_dossier.js >> rapport.txt
 
-echo "############# 2 - Comptez le nombre de documents de la base de données."
+echo "############# 2 - Comptez le nombre de documents de la base de données." >> rapport.txt
 
-mongosh < 2_Nombre_de_documents.js
+mongosh --quiet < js/2_Nombre_de_documents.js >> rapport.txt
 
-echo "############# 3 - Comptez le nombre de logements avec des disponibilités."
+echo "############# 3 - Comptez le nombre de logements avec des disponibilités." >> rapport.txt
 
-mongosh < 3_logements_libres.js
+mongosh --quiet < js/3_logements_libres.js >> rapport.txt
 
-echo "############# 4 - Combien d’annonces y a-t-il par type de location ?"
+echo "############# 4 - Combien d’annonces y a-t-il par type de location ?" >> rapport.txt
 
-mongosh < 4_annonce_par_type.js
+mongosh --quiet < js/4_annonce_par_type.js >> rapport.txt
 
-echo "############# 5 - Quelles sont les 5 annonces de location avec le plus d’évaluations ? Et combien d’évaluations ont-elles ?"
+echo "############# 5 - Quelles sont les 5 annonces de location avec le plus d’évaluations ? Et combien d’évaluations ont-elles ?" >> rapport.txt
 
-mongosh < 5_evaluations.js
+mongosh --quiet < js/5_evaluations.js >> rapport.txt
 
-echo "############# 6 - Quel est le nombre total d’hôtes différents ?"
+echo "############# 6 - Quel est le nombre total d’hôtes différents ?" >> rapport.txt
 
-mongosh < 6_hotes_differents.js
+mongosh --quiet < js/6_hotes_differents.js >> rapport.txt
 
-echo "############# 7 - Quel est le nombre de locations réservables instantanément ? Cela représente quelle proportion des annonces ?"
+echo "############# 7 - Quel est le nombre de locations réservables instantanément ? Cela représente quelle proportion des annonces ?" >> rapport.txt
 
-mongosh < 7_locations_instantanees.js
+mongosh --quiet < js/7_locations_instantanees.js >> rapport.txt
 
-echo "############# 8 - Est-ce que des hôtes ont plus de 100 annonces sur les plateformes ? Et si oui qui sont-ils ? Cela représente quel pourcentage des hôtes ?"
+echo "############# 8 - Est-ce que des hôtes ont plus de 100 annonces sur les plateformes ? Et si oui qui sont-ils ? Cela représente quel pourcentage des hôtes ?" >> rapport.txt
 
-mongosh < 8_hotes_100_annonces.js
+mongosh --quiet < js/8_hotes_100_annonces.js >> rapport.txt
 
-echo "############# 9 - Combien y a-t-il de super hôtes différents ? Cela représente quel pourcentage des hôtes ?"
+echo "############# 9 - Combien y a-t-il de super hôtes différents ? Cela représente quel pourcentage des hôtes ?" >> rapport.txt
 
-mongosh < 9_supers_hotes.js
-
-    
-    
-    
-    
-
-
+mongosh --quiet < js/9_supers_hotes.js >> rapport.txt

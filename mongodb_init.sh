7cec4a44d659921bfd08545daf1d5a239d2c4d08
Vincent <vincent@debian-BULLSEYE-live-builder-AMD64>
2026-04-19 16:56:49 +0200

    correction erreur

--- mongodb_init.sh
+++ mongodb_init.sh
@@ -6,12 +6,12 @@ MONGODB_READER_PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 20; echo)
 MONGO_DB_JSON="mongodb_init.json"
 
 echo "use admin" > $MONGO_DB_JSON
-echo 'db.createUser({user:"admin",pwd:"'${MONGODB_PASSWORD}'",roles:[{role:"root",db:"admin"}]});' >> $MONGO_DB_JSON
+echo 'db.createUser({user:"admin",pwd:"'${MONGODB_ADMIN_PASSWORD}'",roles:[{role:"root",db:"admin"}]});' >> $MONGO_DB_JSON
 mongosh -f $MONGO_DB_JSON
 
 sed -e "s/#######MONGODB_ADMIN_USER#######/${MONGODB_ADMIN_USER}/g" params.ini.model > params.ini
-sed -i -e "s/#######${MONGODB_ADMIN_PASSWORD}#######/asd/g" params.ini
-sed -i -e "s/#######${MONGODB_HOST}#######/asd/g" params.ini
-sed -i -e "s/#######${MONGODB_READER_USER}#######/asd/g" params.ini
-sed -i -e "s/#######${MONGODB_READER_PASSWORD}#######/asd/g" params.ini
+sed -i -e "s/#######MONGODB_ADMIN_PASSWORD#######/${MONGODB_ADMIN_PASSWORD}/g" params.ini
+sed -i -e "s/#######MONGODB_HOST#######/${MONGODB_HOST}/g" params.ini
+sed -i -e "s/#######MONGODB_READER_USER#######/${MONGODB_READER_USER}/g" params.ini
+sed -i -e "s/#######MONGODB_READER_PASSWORD#######/${MONGODB_READER_PASSWORD}/g" params.ini
 

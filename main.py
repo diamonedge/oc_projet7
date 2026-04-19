15b53b443afff1cd00a96fb38bfc35b391401465
Vincent <vincent@debian-BULLSEYE-live-builder-AMD64>
2026-04-19 17:38:57 +0200

    modification indentation

--- main.py
+++ main.py
@@ -191,32 +191,32 @@ def ensure_readonly_user(mongo_uri: str, username: str, password: str) -> Dict[s
 
 if __name__ == "__main__":
 	setup_logging(level="INFO", log_file="app.log")
-    logging.info("Initialisation de la configuration")
-    config = configparser.ConfigParser()
-    config.read('params.ini')
+	logging.info("Initialisation de la configuration")
+	config = configparser.ConfigParser()
+	config.read('params.ini')
 
-    logging.info("Etape 1 - Listing des fichiers à injecter")
-    liste_de_fichier=charger_csv_dans_dictionnaire(config['DEFAULT']['TempDir'])
+	logging.info("Etape 1 - Listing des fichiers à injecter")
+	liste_de_fichier=charger_csv_dans_dictionnaire(config['DEFAULT']['TempDir'])
 
-    logging.info(f"Fin etape 1")
-    print(liste_de_fichier)
+	logging.info(f"Fin etape 1")
+	print(liste_de_fichier)
 
-    logging.info("Etape 2 - Connexion et paramétrage")
-    ensure_db_and_collection(config['DEFAULT']['MongoDbUri'], config['DEFAULT']['Db_name'], config['DEFAULT']['Collection_Name'])
-    ensure_readonly_user(config['DEFAULT']['MongoDbUri'], config['USERS_ROLES']['READER_USER_NAME'], config['USERS_ROLES']['READER_USER_PASSWORD'])
+	logging.info("Etape 2 - Connexion et paramétrage")
+	ensure_db_and_collection(config['DEFAULT']['MongoDbUri'], config['DEFAULT']['Db_name'], config['DEFAULT']['Collection_Name'])
+	ensure_readonly_user(config['DEFAULT']['MongoDbUri'], config['USERS_ROLES']['READER_USER_NAME'], config['USERS_ROLES']['READER_USER_PASSWORD'])
 
-    logging.info("Etape 3 - Injection")
-    n = insert_file_in_batches(
-        mongo_uri=config['DEFAULT']['MongoDbUri'],
-        db_name=config['DEFAULT']['Db_name'],
-        collection_name=config['DEFAULT']['Collection_Name'],
-        file_path=liste_de_fichier[0]["chemin_complet"],
-        batch_size=int(config['DEFAULT']['BatchSize']),
-        ordered=False,
-        delimiter=",",
-    )
-    
-    logging.info(f"Fin étape 3 - documents injectés : {n}")
+	logging.info("Etape 3 - Injection")
+	n = insert_file_in_batches(
+		mongo_uri=config['DEFAULT']['MongoDbUri'],
+		db_name=config['DEFAULT']['Db_name'],
+		collection_name=config['DEFAULT']['Collection_Name'],
+		file_path=liste_de_fichier[0]["chemin_complet"],
+		batch_size=int(config['DEFAULT']['BatchSize']),
+		ordered=False,
+		delimiter=",",
+	)
+
+	logging.info(f"Fin étape 3 - documents injectés : {n}")
 
     #if (n+1)==number_of_lines:
     #    print("Injection terminée avec succès")

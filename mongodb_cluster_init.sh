efb5ff90baf3f9a8d14737c227751916759ee2de
Vincent <vincent@debian-BULLSEYE-live-builder-AMD64>
2026-04-26 16:48:53 +0200

    changement de la configuration cluster

---
+++ mongodb_cluster_init.sh
@@ -0,0 +1,60 @@
+docker compose up -d
+
+echo "lancement de la configuration du premier serveur"
+
+docker exec -it cfg1 mongosh --eval '
+rs.initiate({
+  _id: "cfgRS",
+  configsvr: true,
+  members: [
+    { _id: 0, host: "cfg1:27018" }
+  ]
+})
+'
+
+docker exec -it shard1 mongosh --eval '
+rs.initiate({
+  _id: "shard1RS",
+  members: [
+    { _id: 0, host: "shard1:27018" }
+  ]
+})
+'
+
+docker exec -it shard1 mongosh --eval '
+rs.initiate({
+  _id: "shard2RS",
+  members: [
+    { _id: 0, host: "shard1:27018" }
+  ]
+})
+'
+
+docker exec -it cfg1 mongosh --eval 'rs.status().myState'
+docker exec -it shard1 mongosh --eval 'rs.status().myState'
+docker exec -it shard2 mongosh --eval 'rs.status().myState'
+
+docker exec -it mongos mongosh --eval '
+sh.addShard("shard1RS/shard1:27018");
+sh.addShard("shard2RS/shard2:27018");
+sh.status();
+'
+
+docker exec -it mongos mongosh --eval '
+sh.enableSharding("NosCites");
+'
+
+docker exec -it mongos mongosh --eval '
+sh.enableSharding("NosCites");
+'
+
+docker exec -it mongos mongosh --eval '
+use NosCites;
+db.listing_paris.createIndex({ id: "hashed" });
+sh.shardCollection("NosCites.listing_paris", { id: "hashed" });
+'
+
+docker exec -it cfg1 mongosh --eval 'rs.status().myState'
+docker exec -it shard1 mongosh --eval 'rs.status().myState'
+docker exec -it shard2 mongosh --eval 'rs.status().myState'
+
